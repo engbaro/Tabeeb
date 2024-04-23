7c63d58043dd7926e00031cce9d7893eed028a68
Dara Adekore <adekore8@gmail.com>
2024-04-22 20:54:09 -0700

    Added login for patients and doctors. Can update and close #1

--- app/screens/Doctorlogin.tsx
+++ app/screens/Doctorlogin.tsx
@@ -1,46 +1,72 @@
-import { View, Text, TextInput, Image, StyleSheet, Button } from 'react-native'
+import { View, Text, TextInput, Image, StyleSheet, Button } from "react-native";
 import stethoscope from "@/assets/images/stethoscope.png";
-import { useState } from 'react';
-import Colors from '@/constants/Colors';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useState } from "react";
+import Colors from "@/constants/Colors";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import PhoneInput from "react-native-international-phone-number";
 
-const Doctorlogin = ({navigation}) => {
-    const [email, onChangeEmail] = useState("");
-    const [password, onChangePassword] = useState("");
-    const stethoscope_uri = Image.resolveAssetSource(stethoscope).uri;
-    const insets = useSafeAreaInsets();
-    return (
-        <View style={{ ...styles.container, paddingTop: insets.top, paddingLeft:insets.left,paddingRight:insets.right, paddingBottom:insets.bottom}}>
-          <Image source={{ uri: stethoscope_uri }} style={styles.image} />
-          <TextInput
-            placeholder="email"
-            onChangeText={(text) => onChangeEmail(text)}
-            value={email}
-            style={styles.email}
-          ></TextInput>
-          <TextInput value={password} onChangeText={(text) => onChangePassword(text)} secureTextEntry={true} placeholder="password" style={styles.password}></TextInput>
-          <Button title="Login as a patient" onPress={() => navigation.navigate("screens/Patientlogin")}>Login as a patient</Button>
-        </View>
-      );
+const Doctorlogin = ({ navigation }) => {
+  const [email, onChangeEmail] = useState("");
+  const [password, onChangePassword] = useState("");
+  const stethoscope_uri = Image.resolveAssetSource(stethoscope).uri;
+  const insets = useSafeAreaInsets();
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [selectedCountry, setSelectedCountry] = useState(null);
+  function handleInputValue(phoneNumber) {
+    setPhoneNumber(phoneNumber);
+  }
+  function handleSelectedCountry(country) {
+    setSelectedCountry(country);
+  }
+  return (
+    <View
+      style={{
+        ...styles.container,
+        paddingTop: insets.top,
+      }}
+    >
+      <Image source={{ uri: stethoscope_uri }} style={styles.image} />
+      <PhoneInput
+        value={phoneNumber}
+        onChangePhoneNumber={handleInputValue}
+        selectedCountry={selectedCountry}
+        onChangeSelectedCountry={handleSelectedCountry}
+        placeholder="Enter your phone number"
+        style={styles.phoneinput}
+      />
+      <TextInput
+        value={password}
+        onChangeText={(text) => onChangePassword(text)}
+        secureTextEntry={true}
+        placeholder="password"
+        style={styles.password}
+      ></TextInput>
+      <Button
+        title="Login as a patient"
+        onPress={() => navigation.navigate("screens/Patientlogin")}
+      >
+        Login as a patient
+      </Button>
+    </View>
+  );
 };
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: "center",
-      backgroundColor: Colors.background,
-      gap: 50,
-      objectFit:"scale-down"
-    },
-    email: {
-      fontSize: 25,
-      
-    },
-    password: {
-      fontSize:25,
-    },
-    image: {
-      width: 150,
-      height: 150,
-    },
-  });
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: Colors.background,
+    gap: 50,
+    objectFit: "scale-down",
+  },
+  phoneinput: {
+    fontSize: 25,
+  },
+  password: {
+    fontSize: 25,
+  },
+  image: {
+    width: 150,
+    height: 150,
+  },
+});
 export default Doctorlogin;
--- app/screens/Patientlogin.tsx
+++ app/screens/Patientlogin.tsx
@@ -1,27 +1,59 @@
-import { Text, TextInput, View, StyleSheet, Image , Button} from "react-native";
+import { Text, TextInput, View, StyleSheet, Image, Button } from "react-native";
 import Colors from "@/constants/Colors";
 import { useState } from "react";
-import patient from '@/assets/images/patient.png'
+import patient from "@/assets/images/patient.png";
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
+import PhoneInput from "react-native-international-phone-number";
 
-import { SafeAreaProvider, useSafeAreaInsets } from "react-native-safe-area-context";
-const Patientlogin = ({navigation}) => {
+const Patientlogin = ({ navigation }) => {
   const [email, onChangeEmail] = useState("");
   const [password, onChangePassword] = useState("");
   const patient_uri = Image.resolveAssetSource(patient).uri;
   const insets = useSafeAreaInsets();
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [selectedCountry, setSelectedCountry] = useState(null);
 
-
+  function handleInputValue(phoneNumber) {
+    setPhoneNumber(phoneNumber);
+  }
+  function handleSelectedCountry(country) {
+    setSelectedCountry(country);
+  }
   return (
-    <View style={{ ...styles.container, paddingTop: insets.top, paddingLeft:insets.left,paddingRight:insets.right, paddingBottom:insets.bottom}}>
+    <View
+      style={{
+        ...styles.container,
+        paddingTop: insets.top,
+        paddingLeft: insets.left,
+        paddingRight: insets.right,
+        paddingBottom: insets.bottom,
+      }}
+    >
       <Image source={{ uri: patient_uri }} style={styles.image} />
+      <PhoneInput
+        value={phoneNumber}
+        onChangePhoneNumber={handleInputValue}
+        selectedCountry={selectedCountry}
+        onChangeSelectedCountry={handleSelectedCountry}
+        placeholder="Enter your phone number"
+        style={{ width: "80%" }}
+      />
       <TextInput
-        placeholder="email"
-        onChangeText={(text) => onChangeEmail(text)}
-        value={email}
-        style={styles.email}
+        value={password}
+        onChangeText={(text) => onChangePassword(text)}
+        secureTextEntry={true}
+        placeholder="password"
+        style={styles.password}
       ></TextInput>
-      <TextInput value={password} onChangeText={(text) => onChangePassword(text)} secureTextEntry={true} placeholder="password" style={styles.password}></TextInput>
-      <Button title="Login as a doctor" onPress={() => navigation.navigate("screens/Doctorlogin")}>Login as a patient</Button>
+      <Button
+        title="Login as a doctor"
+        onPress={() => navigation.navigate("screens/Doctorlogin")}
+      >
+        Login as a patient
+      </Button>
     </View>
   );
 };
@@ -32,14 +64,13 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: Colors.background,
     gap: 50,
-    objectFit:"scale-down"
+    objectFit: "scale-down",
   },
   email: {
     fontSize: 25,
-    
   },
   password: {
-    fontSize:25,
+    fontSize: 25,
   },
   image: {
     width: 150,
